2883f504f3242eddb658249f533d8745814207dc
redRockmengxin <1065164542@qq.com>
2022-03-26 11:13:37 +0800

    use for loop to print the dictionary

---
+++ 4forDictionary.py
@@ -0,0 +1,10 @@
+characterA = {'strength':1,
+              'dexterity':2,
+              'constitution':3,
+              'intelligence':4,
+              'wisdom':5,
+              'charisma':6}
+print(characterA)
+for key in characterA.items():
+    print(key)
+
